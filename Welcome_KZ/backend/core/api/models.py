3a1d69807c3e70df56bcb8e1097420902123599d
abilmansur <aliaidarmm0@gmail.com>
2026-04-17 18:43:24 +0500

    update backend part

--- Welcome_KZ/backend/core/api/models.py
+++ Welcome_KZ/backend/core/api/models.py
@@ -1,23 +1,49 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
-
-class Tourist(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    name = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100)
-    phone_number = models.CharField(max_length=11)
-    email_address = models.EmailField()
-    profile_image = models.ImageField()
-    birth_date = models.DateTimeField()
+class Company(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='owner_company')
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    is_verified = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
-    gender_party = [
-        ('M', 'Male'),
-        ('F', 'Female'),
-        ('O', 'Other')
-    ]
-    gender = models.CharField(max_length=1, choices=gender_party, null=True, blank=True)
 
     def __str__(self):
-        return f"Name: {self.name}\nSurname: {self.surname}"
+        return self.name
+
+
+class Profile(models.Model):
+    ROLES_CHOICES = [
+        ('tourist', 'Tourist'),
+        ('guide', 'Guide'),
+        ('owner', 'Company Owner'),
+    ]
+    
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLES_CHOICES, default='tourist')
+    phone_number = models.CharField(max_length=20)
+    profile_image = models.ImageField(upload_to="profiles/", null=True, blank=True)
+
+    company = models.ForeignKey(Company, on_delete=models.SET_NULL, null=True, blank=True, related_name='guides')
+    rating = models.FloatField(default=5.0, validators=[MinValueValidator(0), MaxValueValidator(5)])
+    bio = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
+    
+class EmploymentRequest(models.Model):
+    STATUS_CH = [
+        ('pending', 'Pending'),
+        ("accepted", "Accepted"),
+        ('rejected', "Rejected"),
+    ]
+
+    guide = models.ForeignKey(User, on_delete=models.CASCADE, related_name="job application")
+    company = models.ForeignKey(Company, on_delete=models.CASCADE, related_name='recieved application')
+    status = models.CharField(max_length=10, choices=STATUS_CH, default='pending')
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.guide.username} -> {self.company.name} ({self.status})"
